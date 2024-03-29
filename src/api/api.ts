ff88f398db1d519805e91af644d0edfd54a5b351
vigor13 <vigor13.dev@gmail.com>
2024-03-29 22:32:24 +0900

    feat: add api client

---
+++ src/api/api.ts
@@ -0,0 +1,42 @@
+import qs from "qs";
+
+export interface FetchResponse<T> extends Response {
+  data: T;
+}
+
+export interface APIProps {
+  endpoint: string | URL;
+  options?: RequestInit;
+  queryParams?: any;
+}
+
+export const api = async <Data>(
+  props: APIProps
+): Promise<FetchResponse<Data>> => {
+  const _baseUrl = process.env.NEXT_PUBLIC_API_SERVER_URL;
+  const _apiKey = process.env.NEXT_PUBLIC_API_ACCESS_KEY;
+  const { endpoint, options, queryParams } = props;
+
+  const queryString = qs.stringify(queryParams);
+  const url = `${_baseUrl}/${endpoint}?${queryString}`;
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Client-ID ${_apiKey}`,
+    "Accept-Version": "v1",
+    ...options?.headers,
+  };
+  delete options?.headers;
+
+  const response = await fetch(url, {
+    headers,
+    ...options,
+  });
+
+  if (response.status >= 400) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+
+  return { ...response, data };
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import { HomeScreen } from "@components";
+import { getPhotos } from "@api";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const { data } = await getPhotos();
+  console.log(data);
   return <HomeScreen />;
 }
