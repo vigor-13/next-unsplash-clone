c3a7160e5eb0b879c591599c518019a3121583ec
vigor13 <vigor13.dev@gmail.com>
2024-04-07 21:40:39 +0900

    fix: remove some Grid component props

--- src/components/elements/Grid/Grid.tsx
+++ src/components/elements/Grid/Grid.tsx
@@ -2,34 +2,12 @@ import React from 'react';
 import classNames from 'classnames';
 import { SystemComponentProps } from '@components';
 
-export interface GridProps extends SystemComponentProps {
-  cols?: number;
-  rows?: number;
-  gap?: number;
-  justify?:
-    | 'normal'
-    | 'start'
-    | 'end'
-    | 'center'
-    | 'between'
-    | 'around'
-    | 'evenly'
-    | 'stretch';
-  items?: 'start' | 'end' | 'center' | 'baseline' | 'stretch';
-}
+export interface GridProps extends SystemComponentProps {}
 
 export const Grid: React.FC<GridProps> = (props) => {
-  const { className, children, cols, rows, gap, justify, items } = props;
+  const { className, children } = props;
 
-  const gridClasses = classNames(
-    'grid',
-    cols && `grid-cols-${cols}`,
-    rows && `grid-rows-${rows}`,
-    gap && `gap-${gap}`,
-    justify && `justify-${justify}`,
-    items && `items-${items}`,
-    className,
-  );
+  const gridClasses = classNames('grid', className);
 
   return <div className={gridClasses}>{children}</div>;
 };
