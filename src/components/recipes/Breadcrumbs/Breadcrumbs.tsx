1fbc794ed26cbc1c34864d54288acb3a668d1f9b
vigor13 <vigor13.dev@gmail.com>
2024-04-07 23:05:34 +0900

    feat: add Breadcrumbs component

---
+++ src/components/recipes/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text } from '@components';
+
+interface BreadcrumbsProps {
+  data: string[];
+}
+
+export const Breadcrumbs: React.FC<BreadcrumbsProps> = (props) => {
+  const { data } = props;
+
+  const _isLastItem = (i: number) => {
+    return i === data.length - 1;
+  };
+
+  return (
+    <ul className="flex">
+      {data.map((v, i) => {
+        return (
+          <li key={i} className="flex">
+            <Text
+              className={_isLastItem(i) ? 'text-stone-600' : 'text-stone-400'}
+            >
+              {v}
+            </Text>
+            {!_isLastItem(i) && (
+              <Text className="text-stone-400 px-2">{' › '}</Text>
+            )}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
--- src/components/recipes/index.ts
+++ src/components/recipes/index.ts
@@ -1,3 +1,4 @@
 export * from './MainNavigation';
 export * from './UserProfile';
 export * from './PhotoCard';
+export * from './Breadcrumbs';
