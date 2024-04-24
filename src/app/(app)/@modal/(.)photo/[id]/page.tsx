59d6fab4371f669745fa443ed6f3eef46275989d
= <=>
2024-04-24 18:59:03 +0900

    fix: modal close bug fix

--- src/app/(app)/@modal/(.)photo/[id]/page.tsx
+++ src/app/(app)/@modal/(.)photo/[id]/page.tsx
@@ -1,6 +1,15 @@
+'use client';
 import { Modal, PhotoScreen } from '@components';
+import { usePathname } from 'next/navigation';
 
 export default function Page() {
+  /**
+   * TODO: catch all 조사해보기
+   * @REF: https://github.com/vercel/next.js/discussions/50284#discussioncomment-5992116
+   */
+  const pathname = usePathname();
+  if (!pathname.includes('/photo/')) return null;
+
   return (
     <Modal>
       <PhotoScreen />
