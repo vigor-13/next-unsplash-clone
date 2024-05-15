60f7dcc36546ca6f710fe95429dc4040a85d694b
= <=>
2024-05-15 21:02:13 +0900

    fix: modify css to adjust full-screen height

--- src/components/screens/HomeScreen/HomeScreen.tsx
+++ src/components/screens/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, MainBanner, MainPhotoList, Container } from '@/components';
+import { Box, MainBanner, MainPhotoList } from '@/components';
 
 export interface HomeScreenProps {}
 
